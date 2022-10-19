282c2e35f545bb82891b77ab7c08d98a0e5b3117
sakib2007 <74136713+sakib2007@users.noreply.github.com>
2022-10-19 09:34:00 +0530

    Add files via upload

---
+++ PRO-C58-TEAM-VOTING-APP-main/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCbVpnoTETj1M4rFBqhrZTGczr-wYNnAXQ",
+    authDomain: "team-voting-app-7a18c.firebaseapp.com",
+    databaseURL: "https://team-voting-app-7a18c-default-rtdb.firebaseio.com",
+    projectId: "team-voting-app-7a18c",
+    storageBucket: "team-voting-app-7a18c.appspot.com",
+    messagingSenderId: "966982137530",
+    appId: "1:966982137530:web:af2105a1746751fc5b3483",
+    measurementId: "G-TNTCVTV5W6"
+  };
